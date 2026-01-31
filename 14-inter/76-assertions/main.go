e1a78bb7169af602d2275048841a0db3846ce43e
Mario Lazzari <mario.lazzari@gmail.com>
2026-01-31 10:05:05 +0100

    Done lesson 77

---
+++ 14-inter/76-assertions/main.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type circle struct {
+	radius float64
+}
+type triangle struct {
+	a, b, c float64 // lengths of the sides of a triangle.
+}
+type rectangle struct {
+	h, w float64
+}
+
+func (c circle) String() string {
+	return fmt.Sprint("Circle (Radius: ", c.radius, ")")
+}
+func (t triangle) String() string {
+	return fmt.Sprint("Triangle (Sides: ", t.a, ", ", t.b, ", ", t.c, ")")
+}
+func (r rectangle) String() string {
+	return fmt.Sprint("Rectangle (Sides: ", r.h, ", ", r.w, ")")
+}
+
+func (c circle) area() float64 {
+	return math.Pi * c.radius * c.radius
+}
+
+func (t triangle) area() float64 {
+	// Heron's formula
+	p := (t.a + t.b + t.c) / 2.0
+	return math.Sqrt(p * (p - t.a) * (p - t.b) * (p - t.c))
+}
+
+func (r rectangle) area() float64 {
+	return r.h * r.w
+}
+
+func (t triangle) angles() []float64 {
+	return []float64{angle(t.b, t.c, t.a), angle(t.a, t.c, t.b), angle(t.a, t.b, t.c)}
+}
+
+func angle(a, b, c float64) float64 {
+	return math.Acos((a*a+b*b-c*c)/(2*a*b)) * 180.0 / math.Pi
+}
+
+type Shape interface {
+	area() float64
+}
+
+func main() {
+	// Instantiate circle, triangle, rectangle with values
+	shapes := []Shape{circle{1.0}, triangle{1, 2, 3}, rectangle{2, 3}}
+
+	for _, shape := range shapes {
+		// Print area of each.
+		fmt.Println(shape.area())
+
+		// print angles of the triangle
+		t, ok := shape.(triangle)
+		if ok {
+			fmt.Println(t.angles())
+		}
+	}
+
+}
